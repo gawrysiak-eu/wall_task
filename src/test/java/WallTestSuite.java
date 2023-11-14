500414a02cf769d55ba3a6d476fe1d36a8eb245e
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2023-11-14 21:09:39 +0100

    refactoring in tests, InMemoryWallFactory added

--- src/test/java/WallTestSuite.java
+++ src/test/java/WallTestSuite.java
@@ -1,10 +1,6 @@
 
-
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,32 +8,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class WallTestSuite {
 
-    private static List<Block> blocks;
-
-    @BeforeAll
-    static void createTestData() {
-        blocks = new ArrayList<>();
-        blocks.add(new NormalBlock("Gray", "Metal"));
-        blocks.add(new NormalBlock("Gray", "Cement"));
-        blocks.add(new NormalBlock("Brown", "Wood"));
-        List<Block> multiList = List.of(
-                new NormalBlock("Blue", "Plastic"),
-                new NormalBlock("Blue", "Plastic"),
-                new NormalBlock("Blue", "Plastic"));
-        MultiBlock multi1 = new MultiBlock("Blue", "Plastic", multiList);
-        blocks.add(multi1);
-        List<Block> multiList2 = List.of(
-                new NormalBlock("Yellow", "Gold"),
-                new NormalBlock("Yellow", "Gold"),
-                new NormalBlock("Yellow", "Gold"));
-        MultiBlock multi2 = new MultiBlock("Yellow", "Gold", multiList2);
-        blocks.add(multi2);
-    }
 
     @Test
     void testFindBlockByColor() {
         // given
-        Wall wall = new Wall(blocks);
+        Wall wall = InMemoryWallFactory.getWall();
         // when
         Optional<Block> grayTest = wall.findBlockByColor("Gray");
         Optional<Block> pinkTest = wall.findBlockByColor("Pink");
@@ -50,7 +25,7 @@ public class WallTestSuite {
     @Test
     void testFindBlocksByMaterial() {
         // given
-        Wall wall = new Wall(blocks);
+        Wall wall = InMemoryWallFactory.getWall();
         // when
         List<Block> gold = wall.findBlocksByMaterial("Gold");
         List<Block> platinum = wall.findBlocksByMaterial("Platinum");
@@ -62,7 +37,7 @@ public class WallTestSuite {
     @Test
     void testCount() {
         // given
-        Wall wall = new Wall(blocks);
+        Wall wall = InMemoryWallFactory.getWall();
         // when
         int countResult = wall.count();
         // then
@@ -72,7 +47,7 @@ public class WallTestSuite {
     @Test
     void testFindBlockByColorThrowException() {
         // given
-        Wall wall = new Wall(blocks);
+        Wall wall = InMemoryWallFactory.getWall();
         // when and then
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> wall.findBlockByColor(null));
         assertEquals("Color cannot be null", exception.getMessage());
@@ -81,7 +56,7 @@ public class WallTestSuite {
     @Test
     void testFindBlocksByMaterialThrowException() {
         // given
-        Wall wall = new Wall(blocks);
+        Wall wall = InMemoryWallFactory.getWall();
         // when and then
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> wall.findBlocksByMaterial(null));
         assertEquals("Material cannot be null", exception.getMessage());
